d6361d0ce136edbbaa521c8f0ae2c51bc8acc89e
Olivia Roche <livroche2103@gmail.com>
2018-02-02 11:19:34 +0000

    Global

---
+++ E-Business/cost_calc.js
@@ -0,0 +1,29 @@
+/* global $ */
+
+function calcSub(){
+var argSubTotal;
+
+if(document.getElementById("salesforce").checked){  
+    argSubTotal= 100;
+    }
+    else{
+        argSubTotal = 300;
+    }
+    display(argSubTotal);
+}
+
+
+function display(parm1){
+    
+    document.getElementById("Subtotal").value = parm1;
+      document.getElementById("Total").value = parm1;
+      
+      enablebtnProceed();
+}
+
+function enablebtnProceed(){
+    $("btnProceed").prop("disabled",false);
+}
+    function enablebtnProceed(){
+    $("btnProceed").prop("disabled",true);
+}
